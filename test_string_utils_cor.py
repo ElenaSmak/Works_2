e3cd82a1d79cd3da52ec0e0d094e6d0d77505d3e
ElenaSmak <163130156+ElenaSmak@users.noreply.github.com>
2024-05-28 14:55:11 +0300

    Исправленная Домашка 4

--- test_string_utils_cor.py
+++ test_string_utils_cor.py
@@ -1,17 +1,24 @@
 import pytest
 from string_utils import StringUtils
     
-@pytest.mark.parametrize('string, result', [('звезда', 'Звезда'), ('идет дождь', 'Идет дождь'), ('',''), (' ', ' ')])   
+@pytest.mark.parametrize('string, result', [('звезда', 'Звезда'), ('идет дождь', 'Идет дождь'), ('1000', '1000')])  # доработка домашки  
 def test_capitilize_positive(string, result):
     string_utils = StringUtils()
     res = string_utils.capitilize(string)
     assert res == result
-       
-@pytest.mark.xfail
+
+@pytest.mark.xfail  # доработка домашки 
 def test_capitilize_negative1():
     string_utils = StringUtils() 
-    res = string_utils.capitilize('1000')
-    assert res == '1000'
+    res = string_utils.capitilize('')
+    assert res == ''   
+
+@pytest.mark.xfail  # доработка домашки 
+def test_capitilize_negative1():
+    string_utils = StringUtils() 
+    res = string_utils.capitilize(' ')
+    assert res == ' '       
+       
 
 @pytest.mark.xfail
 def test_capitilize_negative3():
@@ -67,7 +74,7 @@ def test_to_list_negative2():
     res = string_utils.to_list(None)
     assert res == None     
 
-@pytest.mark.parametrize('string, symbol, result', [('Дождь','д', True), ('Дождь', 'а', False)])  
+@pytest.mark.parametrize('string, symbol, result', [('Дождь','д', True), ('Дождь', 'а', False), ('1000', '1', True)])  # доработка домашки  
 def test_contains_positive(string, symbol, result):
     string_utils = StringUtils()
     res = string_utils.contains(string, symbol)
@@ -89,10 +96,35 @@ def test_contains_negative2():
 def test_delete_symbol_positive(string, symbols, result):
     string_utils = StringUtils()
     res = string_utils.contains(string, symbols)
-    assert res == result     
-
+    assert res == result  
+      
 @pytest.mark.parametrize('string, symbol, result', [('Дождь','Д', True), ('Дождь', 'х', False)])  
 def test_starts_with_positive(string, symbol, result):
     string_utils = StringUtils()
     res = string_utils.starts_with(string, symbol)
     assert res == result  
+
+# доработка домашки - все тесты ниже
+@pytest.mark.parametrize('string, symbol, result', [('ОКо','о', 'ОК'), ('Дождть', 'т','Дождь' )])  
+def test_delete_symbol_fix(string, symbol, result):
+    string_utils = StringUtils()
+    res = string_utils.delete_symbol(string, symbol)
+    assert res == result     
+
+@pytest.mark.parametrize('string, symbol, result', [('Дождь','ь', True), ('Дождь', 'х', False)])  
+def test_end_with_positive(string, symbol, result):
+    string_utils = StringUtils()
+    res = string_utils.end_with(string, symbol)
+    assert res == result      
+
+@pytest.mark.parametrize('string, result', [('  ', True), ('Дождь', False)])  
+def test_is_empty_positive(string, result):
+    string_utils = StringUtils()
+    res = string_utils.is_empty(string)
+    assert res == result      
+
+@pytest.mark.parametrize('list, joiner, result', [([1,2,3,4], ',', '1, 2, 3, 4'), (['дождь', 'снег'], ';', 'дождь; снег' )])  
+def list_to_string_positive(list, joiner, result):
+    string_utils = StringUtils()
+    res = string_utils.list_to_string(list, joiner)
+    assert res == result    
